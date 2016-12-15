ffa1d5c7c5f89b64527a1cebdd4dee21e8719931
Hsin-Fang Chiang <hchiang2@illinois.edu>
2016-12-15 09:18:59 -0600

    Add site lsstvc for running the workflow on LSST Verification Cluster

--- plan_dax.sh
+++ plan_dax.sh
@@ -11,15 +11,13 @@ DAXFILE=$1
 
 # This command tells Pegasus to plan the workflow contained in 
 # dax file passed as an argument. The planned workflow will be stored
-# in the "submit" directory. The execution # site is "".
-# --input-dir tells Pegasus where to find workflow input files.
-# --output-dir tells Pegasus where to place workflow output files.
-    #-Dpegasus.register=false \
+# in the "submit" directory.
 pegasus-plan \
     -Dpegasus.transfer.links=true \
     -Dpegasus.catalog.site.file=sites.xml \
     -Dpegasus.catalog.transformation.file=tc.txt \
-    --sites local \
+    -Dpegasus.data.configuration=sharedfs \
+    --sites lsstvc \
     --output-dir $DIR/output \
     --dir $DIR/submit \
     --dax $DAXFILE \
