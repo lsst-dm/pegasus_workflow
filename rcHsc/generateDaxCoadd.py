849499a7fc2b12a43ab013c4f5c427a84bdfa23a
Hsin-Fang Chiang <hchiang2@illinois.edu>
2017-07-12 17:48:49 -0500

    Move the hacky getDataFile method to its own file

--- rcHsc/generateDaxCoadd.py
+++ rcHsc/generateDaxCoadd.py
@@ -8,7 +8,7 @@ import lsst.utils
 from lsst.utils import getPackageDir
 from lsst.daf.persistence import Butler
 from lsst.obs.hsc.hscMapper import HscMapper
-from findShardId import findShardId
+from getDataFile import getDataFile
 
 logger = lsst.log.Log.getLogger("workflow")
 logger.setLevel(lsst.log.WARN)
@@ -26,42 +26,6 @@ rootRepo = "/datasets/hsc/repo"
 # This is a config of LoadIndexedReferenceObjectsTask ref_dataset_name
 refcatName = "ps1_pv3_3pi_20170110"
 
-def getDataFile(mapper, datasetType, dataId, create=False, repoRoot=None):
-    """Get the Pegasus File entry given Butler datasetType and dataId.
-    Retrieve the file name/path through a CameraMapper instance
-        and prepend outPath to it
-    Optionally create new Pegasus File entries
-    Parameters
-    ----------
-    mapper: lsst.obs.base.CameraMapper
-        A specific CameraMapper instance for getting the name and locating
-        the file in a Butler repo.
-    datasetType: `str`
-        Butler dataset type
-    dataId: `dict`
-        Butler data ID
-    create: `bool`, optional
-        If True, create a new Pegasus File entry if it does not exist yet.
-    repoRoot: `str`, optional
-        Prepend butler repo root path for the PFN of the input files
-    Returns
-    -------
-    fileEntry:
-        A Pegasus File entry or a LFN corresponding to an entry
-    """
-    mapFunc = getattr(mapper, "map_" + datasetType)
-    butlerPath = mapFunc(dataId).getLocations()[0]
-    fileEntry = lfn = os.path.join(outPath, butlerPath)
-
-    if create:
-        fileEntry = peg.File(lfn)
-        if repoRoot is not None:
-            filePath = os.path.join(repoRoot, butlerPath)
-            #fileEntry.addPFN(peg.PFN(filePath, site="local"))
-            fileEntry.addPFN(peg.PFN(filePath, site="lsstvc"))
-            logger.info("%s %s: %s -> %s", datasetType, dataId, filePath, lfn)
-
-    return fileEntry
 
 def generateCoaddDax(name="dax", tractDataId=0, dataDict=None):
     """Generate a Pegasus DAX abstract workflow"""
