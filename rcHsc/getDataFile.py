ceef24c4fc5e59ea56ad3fa989450bfeaeb37593
Hsin-Fang Chiang <hchiang2@illinois.edu>
2017-07-12 18:02:17 -0500

    Fix getDataFile global variable

--- rcHsc/getDataFile.py
+++ rcHsc/getDataFile.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 import os
+import Pegasus.DAX3 as peg
 import lsst.log
 
 logger = lsst.log.Log.getLogger("getDataFile")
-logger.setLevel(lsst.log.DEBUG)
+logger.setLevel(lsst.log.WARN)
 
-def getDataFile(mapper, datasetType, dataId, create=False, repoRoot=None):
+def getDataFile(mapper, datasetType, dataId, outPath="repo", create=False, repoRoot=None):
     """Get the Pegasus File entry given Butler datasetType and dataId.
     Retrieve the file name/path through a CameraMapper instance
         and prepend outPath to it
@@ -20,6 +21,8 @@ def getDataFile(mapper, datasetType, dataId, create=False, repoRoot=None):
         Butler dataset type
     dataId: `dict`
         Butler data ID
+    outPath: `str`
+        A folder name used as a hack to use the CmdLineTask framework
     create: `bool`, optional
         If True, create a new Pegasus File entry if it does not exist yet.
     repoRoot: `str`, optional
