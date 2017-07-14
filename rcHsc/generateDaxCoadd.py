eb229a211b4a9b7e96179a886aa814eb404dcf6d
Hsin-Fang Chiang <hchiang2@illinois.edu>
2017-07-14 18:17:38 -0500

    (TBS) Remove hard-coded allPatches; read from dataDict instead

--- rcHsc/generateDaxCoadd.py
+++ rcHsc/generateDaxCoadd.py
@@ -254,10 +254,7 @@ def generateCoaddDax(name="dax", tractDataId=0, dataDict=None):
 
             dax.addJob(detectCoaddSources)
 
-    # TODO: this should be input; hard-coded for now
-    # should only care about non-empty patches of the given tract
-    allPatches = ['0,0', '0,1', '0,2', '0,3', '0,4', '0,5', '0,6', '0,7', '0,8', '1,0', '1,1', '1,2', '1,3', '1,4', '1,5', '1,6', '1,7', '1,8', '2,0', '2,1', '2,2', '2,3', '2,4', '2,5', '2,6', '2,7', '2,8', '3,0', '3,1', '3,2', '3,3', '3,4', '3,5', '3,6', '3,7', '3,8', '4,0', '4,1', '4,2', '4,3', '4,4', '4,5', '4,6', '4,7', '4,8', '5,0', '5,1', '5,2', '5,3', '5,4', '5,5', '5,6', '5,7', '5,8', '6,0', '6,1', '6,2', '6,3', '6,4', '6,5', '6,6', '6,7', '6,8', '7,0', '7,1', '7,2', '7,3', '7,4', '7,5', '7,6', '7,7', '7,8', '8,0', '8,1', '8,2', '8,3', '8,4', '8,5', '8,6', '8,7', '8,8']
-    #allPatches = ['4,4']
+    allPatches = list({patch for patch in dataDict[filterName] for filterName in dataDict})
 
     # Pipeline: mergeCoaddDetections per patch
     for patchDataId in allPatches:
