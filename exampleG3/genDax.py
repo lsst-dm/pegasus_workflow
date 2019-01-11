6ed23f7ab8962080c75671fafbb032b6cead200b
Hsin-Fang Chiang <hchiang2@illinois.edu>
2019-01-10 18:07:58 -0600

    Split jobs using visits from sqlite3 query

--- exampleG3/genDax.py
+++ exampleG3/genDax.py
@@ -4,6 +4,7 @@
 
 
 import os
+import sqlite3
 import Pegasus.DAX3 as peg
 
 import lsst.daf.persistence as dafPersist
@@ -22,14 +23,20 @@ dax.addExecutable(pipetask)
 
 
 repoPath = "/scratch/hchiang2/sw/ci_hsc/DATA"
-for visit in range(1, 5):
-    outCollection = "out-test%d" % visit
+conn = sqlite3.connect(os.path.join(repoPath, "gen3.sqlite3"))
+c = conn.cursor()
+inputType = "raw"
+visits = 'select distinct visit from Dataset inner join PosixDatastoreRecords on Dataset.dataset_id = PosixDatastoreRecords.dataset_id where  dataset_type_name = "%s";' % inputType;
+
+for v in c.execute(visits):
+    visit = v[0]
+    outCollection = "outs/out2"
     #inputRaw = peg.File("file.raw.visit%d" % visit)
     #inputRaw.addPFN(peg.PFN(filePath, site="local"))
     #dax.addFile(inputRaw)
     exampleCmdLineTask = peg.Job(name="pipetask")
-    exampleCmdLineTask.addArguments("-b", repoPath, "-i", "raw",
-                                    "-o", outCollection, "run",
+    exampleCmdLineTask.addArguments("-b", repoPath, "-i", inputType, "-o", outCollection,
+                                    "-d", "Visit.visit=%d" % visit, "run",
                                     "-t", "rawToCalexpTask.RawToCalexpTask")
     #exampleCmdLineTask.uses(inputRaw, link=peg.Link.INPUT)
 
@@ -39,7 +46,7 @@ for visit in range(1, 5):
     exampleCmdLineTask.uses(result, link=peg.Link.OUTPUT, transfer=True, register=True)
     dax.addJob(exampleCmdLineTask)
 
-
+conn.close()
 f = open("testdaxfile.dax", "w")
 dax.writeXML(f)
 f.close()
